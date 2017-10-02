f4736071a292c7441b10d2d34053a29d7abbfe35
Nick Otter <nicholas.a.otter@gmail.com>
2017-10-02 14:13:14 +0100

    Added bank initialize method and tests

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,4 +2,10 @@ require 'client'
 require 'formatter'
 
 class Bank
+  attr_reader :client, :formatter
+
+  def initialize(client = Client.new, formatter = Formatter.new)
+    @client = client
+    @formatter = formatter
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,4 +1,12 @@
 require 'bank'
 
 RSpec.describe Bank do
+  context '#init' do
+    it 'initializes with a new instance of Client' do
+      expect(subject.client).to be_an_instance_of(Client)
+    end
+    it 'initializes with a new instance of Formatter' do
+      expect(subject.formatter).to be_an_instance_of(Formatter)
+    end
+  end
 end
