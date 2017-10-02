0cecc472ead282da21e993736e000c984e252e7b
Nick Otter <nicholas.a.otter@gmail.com>
2017-10-02 15:48:18 +0100

    Added client update credit and debit methods and tests

--- lib/client.rb
+++ lib/client.rb
@@ -1,3 +1,5 @@
+DATE = 14/01/2012
+
 # This class stores all bank account information for the client
 class Client
   attr_reader :balance, :date, :debit, :credit
@@ -7,5 +9,15 @@ class Client
     @date = []
     @debit = []
     @credit = []
+  end
+
+  def update_credit(amount)
+    @credit.push(amount)
+    @date.push(DATE)
+  end
+
+  def update_debit(amount)
+    @debit.push(amount)
+    @date.push(DATE)
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe Client do
       expect(subject.credit).to eq([])
     end
   end
+
+  AMOUNT = 10
+  DATE = 14/01/2012
+
+  context '#update_credit' do
+    it 'adds amount to credit array' do
+      subject.update_credit(AMOUNT)
+      expect(subject.credit).to eq([10])
+    end
+    it 'adds date to date array' do
+      subject.update_credit(AMOUNT)
+      expect(subject.date).to eq([DATE])
+    end
+  end
+
+  context '#update_debit' do
+    it 'adds amount to debit' do
+      subject.update_debit(AMOUNT)
+      expect(subject.debit).to eq([10])
+    end
+    it 'adds date to date array' do
+      subject.update_credit(AMOUNT)
+      expect(subject.date).to eq([DATE])
+    end
+  end
+
 end
