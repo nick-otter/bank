c240a9b75f8051c6b004dcc72788796bf095ca6c
Nick Otter <nicholas.a.otter@gmail.com>
2017-10-02 15:27:20 +0100

    Added client initialize method and tests

--- lib/client.rb
+++ lib/client.rb
@@ -1,2 +1,11 @@
+# This class stores all bank account information for the client
 class Client
+  attr_reader :balance, :date, :debit, :credit
+
+  def initialize
+    @balance = []
+    @date = []
+    @debit = []
+    @credit = []
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,4 +1,18 @@
 require 'client'
 
 RSpec.describe Client do
+  context '#init' do
+    it 'initializes with an empty balance array' do
+      expect(subject.balance).to eq([])
+    end
+    it 'initializes with an empty date array ' do
+      expect(subject.date).to eq([])
+    end
+    it 'initializes with an empty debit array ' do
+      expect(subject.debit).to eq([])
+    end
+    it 'initializes with an empty credit array ' do
+      expect(subject.credit).to eq([])
+    end
+  end
 end
